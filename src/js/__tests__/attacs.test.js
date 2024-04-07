e6a0bab514c2d717925f5bdbbdf200d0589ef21a
Growgs <yazikov@yandex.ru>
2024-04-07 15:24:12 +0300

    initial commit

---
+++ src/js/__tests__/attacs.test.js
@@ -0,0 +1,84 @@
+import extractSpecialAttacks from '../attacs.js'
+
+test('default description', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      }, 
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...'
+      }
+    ]	
+  };
+
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      description: 'Двойной выстрел наносит двойной урон',
+      icon: 'http://...'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      description: 'Описание недоступно',
+      icon: 'http://...'
+    }
+  ];
+
+  expect(extractSpecialAttacks(character)).toEqual(expected);
+});
+
+test('aprovided description', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      }, 
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Нокаутирующий удар оглушает противника на 1 ход'
+      }
+    ]	
+  };
+
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      description: 'Двойной выстрел наносит двойной урон',
+      icon: 'http://...'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      description: 'Нокаутирующий удар оглушает противника на 1 ход',
+      icon: 'http://...'
+    }
+  ];
+
+  expect(extractSpecialAttacks(character)).toEqual(expected);
+});
